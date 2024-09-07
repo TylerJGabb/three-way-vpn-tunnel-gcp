c26b331b402d6faf70d150ffd3d0b19561938289
Tyler Gabb <tylerjgabb@gmail.com>
2024-09-06 20:34:10 -0700

    group the ips so the /30 block in link local requirement is visible

--- shared.tf
+++ shared.tf
@@ -1,7 +1,12 @@
 locals {
-  shared_secret         = "shared_secret"
+  shared_secret = "shared_secret"
+
+  # the interface IPs must be in the same /30 CIDR block in the link local space
+  # it is misleading that the terraform resource name is "ip_range,", so I've 
+  # generalized it to "ip" in the local variable name, tacking on the CIDR block
   a-to-b-interface-1-ip = "169.254.0.1"
-  a-to-b-interface-2-ip = "169.254.1.2"
   b-to-a-interface-1-ip = "169.254.0.2"
-  b-to-a-interface-2-ip = "169.254.1.1"
+
+  a-to-b-interface-2-ip = "169.254.2.1"
+  b-to-a-interface-2-ip = "169.254.2.2"
 }
